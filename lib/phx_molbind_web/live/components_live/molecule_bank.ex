2485aa9ad7fca5c04941a2ccc0fd90a0a6011943
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-04-19 18:43:14 +0530

    MoleculeBank

---
+++ lib/phx_molbind_web/live/components_live/molecule_bank.ex
@@ -0,0 +1,15 @@
+defmodule PhxMolbindWeb.ComponentsLive.MoleculeBank do
+  use PhxMolbindWeb, :live_component
+
+  def render(assigns) do
+    ~H"""
+    <div>
+    </div>
+
+    """
+  end
+
+  def update(assigns, socket) do
+    {:ok, socket |> assign(assigns)}
+  end
+end
