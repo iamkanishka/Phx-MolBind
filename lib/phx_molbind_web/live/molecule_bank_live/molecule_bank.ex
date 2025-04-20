be66c14abc76d146f4f551b0d3cb13cb93d0daa6
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-04-20 11:56:18 +0530

    MoleculeBank

---
+++ lib/phx_molbind_web/live/molecule_bank_live/molecule_bank.ex
@@ -0,0 +1,20 @@
+defmodule PhxMolbindWeb.MoleculeBankLive.MoleculeBank do
+  use PhxMolbindWeb, :live_view
+
+  def render(assigns) do
+    ~H"""
+
+    <DefaultLayout>
+      <ComponetHeader pageName="Molecule Bank" containActionButton={true} />
+      <div className="flex flex-col gap-10">
+        <MoleculeBankTable />
+      </div>
+    </DefaultLayout>
+
+    """
+  end
+
+  def mount(params, session, socket) do
+    {:ok, socket}
+  end
+end
