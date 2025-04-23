74d8ae12622cab94d28edf5e5a049cb272967d07
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-04-23 21:32:07 +0530

    chore: Added icon

--- lib/phx_molbind_web/live/components_live/dashboard_live/CTA_card.ex
+++ lib/phx_molbind_web/live/components_live/dashboard_live/CTA_card.ex
@@ -3,22 +3,23 @@ defmodule PhxMolbindWeb.ComponentsLive.DashboardLive.CTACard do
 
   def render(assigns) do
     ~H"""
-        <div className="rounded-lg border border-stroke bg-white px-7.5 py-6 shadow-sm dark:border-[#181818] dark:bg-[#181818] ">
-      <div className="flex h-11.5 w-11.5 items-center justify-center rounded-full bg-meta-2 dark:bg-meta-4">
-        {children}
+    <div class="rounded-lg border border-stroke bg-white px-7.5 py-6 shadow-sm dark:border-[#181818] dark:bg-[#181818] ">
+      <div class="flex h-11.5 w-11.5 items-center justify-center rounded-full bg-meta-2 dark:bg-meta-4">
+        <.icon name={"hero-#{@icon}"} class="h-5 w-5" />
       </div>
 
-      <div className="mt-4 flex items-end justify-between">
+      <div class="mt-4 flex items-end justify-between">
         <div>
-          <h4 className="text-title-md font-semibold text-black dark:text-white">
-            {title}
+          <h4 class="text-title-md font-semibold text-black dark:text-white">
+            {@title}
           </h4>
-          <span className="text-sm font-medium">{subtitle}</span>
+           <span class="text-sm font-medium">{@subtitle}</span>
         </div>
       </div>
-      <div className="mt-2 w-min cursor-pointer rounded-full bg-[#64748b] p-2">
-        <span className=" text-sm text-white ">
-          <ArrowRight size={20} />
+
+      <div class="mt-2 w-min cursor-pointer rounded-full bg-[#64748b] p-2">
+        <span class=" text-sm text-white ">
+          <.icon name="hero-arrow-right" class="h-5 w-5" />
         </span>
       </div>
     </div>
