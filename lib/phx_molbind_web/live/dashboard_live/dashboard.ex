41302a9e2b77b3606c27096254ab5ee0ededac39
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-04-23 21:32:28 +0530

    feat: Added CTA cards

---
+++ lib/phx_molbind_web/live/dashboard_live/dashboard.ex
@@ -0,0 +1,46 @@
+defmodule PhxMolbindWeb.DashboardLive.Dashboard do
+  use PhxMolbindWeb, :live_view
+
+  @impl true
+  def render(assigns) do
+    ~H"""
+    <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
+      <%= for card <- @cta_card_list do %>
+        <.live_component
+          module={PhxMolbindWeb.ComponentsLive.DashboardLive.CTACard}
+          title={card.title}
+          subtitle={card.subtitle}
+          icon={card.icon}
+          id={"cta_card_#{card.title}"}
+        />
+      <% end %>
+    </div>
+    """
+  end
+
+  @impl true
+  def mount(params, session, socket) do
+    {:ok,
+     socket
+     |> assign(:page_title, "Dashboard")
+     |> assign_CTA_card_list()}
+  end
+
+  def assign_CTA_card_list(socket) do
+    socket
+    |> assign(:cta_card_list, [
+      %{title: "Molecule Bank", subtitle: "get access to more molecules", icon: "atom"},
+      %{title: "Generate Molecule", subtitle: "get access to more molecules", icon: "network"},
+      %{
+        title: "Search Compounds",
+        subtitle: "get access to more molecules",
+        icon: "magnifying-glass"
+      },
+      %{
+        title: "Collaborative Research",
+        subtitle: "get access to more molecules",
+        icon: "chat-bubble-oval-left"
+      }
+    ])
+  end
+end
