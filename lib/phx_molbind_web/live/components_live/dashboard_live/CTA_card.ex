afdad8efe406e377cde66c1be8d9432290f3fbd8
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-04-26 22:09:23 +0530

    CTACard

--- lib/phx_molbind_web/live/components_live/dashboard_live/CTA_card.ex
+++ lib/phx_molbind_web/live/components_live/dashboard_live/CTA_card.ex
@@ -3,7 +3,7 @@ defmodule PhxMolbindWeb.ComponentsLive.DashboardLive.CTACard do
 
   def render(assigns) do
     ~H"""
-    <div class="rounded-lg border border-stroke bg-white px-7.5 py-6 shadow-sm dark:border-[#181818] dark:bg-[#181818] ">
+    <div class="rounded-lg border border-stroke bg-white px-10 py-6 shadow-sm dark:border-[#181818] dark:bg-[#181818] ">
       <div class="flex h-11.5 w-11.5 items-center justify-center rounded-full bg-meta-2 dark:bg-meta-4">
         <.icon name={"hero-#{@icon}"} class="h-5 w-5" />
       </div>
