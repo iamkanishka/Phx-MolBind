0ebd34ce369672d2e34d2c63fe9b568704219541
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-04-27 21:13:39 +0530

    feat: Added template SidebarDropdown

--- lib/phx_molbind_web/live/components_live/sidebar/sidebar_dropdown.ex
+++ lib/phx_molbind_web/live/components_live/sidebar/sidebar_dropdown.ex
@@ -1,16 +1,33 @@
 defmodule PhxMolbindWeb.ComponentsLive.Sidebar.SidebarDropdown do
-use PhxMolbindWeb, :live_component
-
+  use PhxMolbindWeb, :live_component
 
   @impl true
   def render(assigns) do
     ~H"""
-    <div class="flex flex-col gap-2 px-4 py-2">
-      <%= for item <- @items do %>
+    <ul class="mb-5.5 mt-4 flex flex-col gap-2.5 pl-6">
+      <%= for {child_item, _child_item_index} <- Enum.with_index(@item) do %>
+        <li>
+          <.link
+            navigate={child_item.route}
+            class={
 
+              "group relative flex items-center gap-2.5 rounded-md px-4 font-medium text-bodydark2 duration-300 ease-in-out hover:text-white" <>
+              if @current_path == child_item.route, do: "text-white", else: ""
+
+          }
+          >
+            {child_item.label}
+          </.link>
+        </li>
       <% end %>
-    </div>
+    </ul>
     """
   end
 
+  @impl true
+  def update(assigns, socket) do
+    {:ok,
+     socket
+     |> assign(assigns)}
+  end
 end
