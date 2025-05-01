40db9ce0c17eec9bfd36098c2ea0ddff39beb96c
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-05-01 21:52:20 +0530

    feat: CreateMolecule migration

---
+++ priv/repo/migrations/20250501054054_create_molecule_optimizations.exs
@@ -0,0 +1,22 @@
+defmodule PhxMolbind.Repo.Migrations.CreateMoleculeOptimizations do
+  use Ecto.Migration
+
+  def change do
+    create table(:molecule_optimizations) do
+      add :algorithm, :string, null: false
+      add :num_molecules, :integer, null: false
+      add :property_name, :string, null: false
+      add :minimize, :boolean, default: false, null: false
+      add :min_similarity, :float, null: false
+      add :particles, :integer, null: false
+      add :iterations, :integer, null: false
+      add :smi, :text, null: false
+      add :generated_molecules, :map, default: %{}
+      add :user_id, references(:users, on_delete: :delete_all), null: false
+
+      timestamps()
+    end
+
+    create index(:molecule_optimizations, [:user_id])
+  end
+end
