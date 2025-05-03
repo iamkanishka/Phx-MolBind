f15744ff9b873a9a824b65a6c4eebdc972c0bcac
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-05-03 21:53:07 +0530

    Users Schema

---
+++ lib/phx_molbind/accounts/user.ex
@@ -0,0 +1,23 @@
+
+defmodule PhxMolbind.Accounts.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :address, :string
+    field :nonce, :string
+    field :username, :string
+    field :metadata, :map, default: %{}
+    field :last_login_at, :utc_datetime_usec
+
+    timestamps(type: :utc_datetime)
+
+  end
+
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:address, :nonce, :username, :metadata, :last_login_at])
+    |> validate_required([:address, :nonce])
+    |> unique_constraint(:address)
+  end
+end
