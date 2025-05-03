08ab9bba89b544e2308a1308823640470f2acdc7
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-05-03 21:55:41 +0530

    CreateMolecule

--- priv/repo/migrations/20250501054054_create_molecule_optimizations.exs
+++ priv/repo/migrations/20250501054054_create_molecule_optimizations.exs
@@ -2,6 +2,19 @@ defmodule PhxMolbind.Repo.Migrations.CreateMolecule do
   use Ecto.Migration
 
   def change do
+    create table(:users) do
+      add :address, :string, null: false
+      add :nonce, :string, null: false
+      add :username, :string
+      add :metadata, :map, default: %{}
+      add :last_login_at, :utc_datetime_usec
+
+      timestamps()
+    end
+
+    create unique_index(:users, [:address])
+
+
     create table(:molecule_optimizations) do
       add :algorithm, :string, null: false
       add :num_molecules, :integer, null: false
