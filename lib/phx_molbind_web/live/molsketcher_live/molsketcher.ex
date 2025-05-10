38780327c6b905964114a2656d12ba6cfa9f1e86
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-05-10 21:21:32 +0530

    MolsketcherLive.Molsketcher

---
+++ lib/phx_molbind_web/live/molsketcher_live/molsketcher.ex
@@ -0,0 +1,15 @@
+defmodule PhxMolbindWeb.MolsketcherLive.Molsketcher do
+  use PhxMolbindWeb, :live_view
+
+  def render(assigns) do
+    ~H"""
+
+
+
+    """
+  end
+
+  def mount(_params, _session, socket) do
+    {:ok, socket}
+  end
+end
