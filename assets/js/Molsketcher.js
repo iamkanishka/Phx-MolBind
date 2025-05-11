d7865ab8cae7e6734636d44ec7fe0bb1ca8e46ac
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-05-11 21:48:42 +0530

    Molsketcher  hook

--- assets/js/Molsketcher.js
+++ assets/js/Molsketcher.js
@@ -1,21 +1,35 @@
 export default {
   mounted() {
-    // Customize Jmol colors
-    ChemDoodle.ELEMENT["H"].jmolColor = "black";
-    ChemDoodle.ELEMENT["S"].jmolColor = "#B9A130";
-
-    // Initialize the sketcher
-    this.sketcher = new ChemDoodle.SketcherCanvas("sketcher", 500, 300, {
-      useServices: true,
-    });
-
-    // Set sketcher styles
+    // Initialize sketcher
+    ChemDoodle.ELEMENT['H'].jmolColor = 'black';
+    ChemDoodle.ELEMENT['S'].jmolColor = '#B9A130';
+    
+    this.sketcher = new ChemDoodle.SketcherCanvas('sketcher', 500, 300, { useServices: true });
     this.sketcher.styles.atoms_displayTerminalCarbonLabels_2D = true;
     this.sketcher.styles.atoms_useJMOLColors = true;
     this.sketcher.styles.bonds_clearOverlaps_2D = true;
-    this.sketcher.styles.shapes_color = "#c10000";
-
-    // Force re-render
+    this.sketcher.styles.shapes_color = '#000000';
     this.sketcher.repaint();
-  },
-};
+
+    // Handle export button
+    this.handleEvent("get_molecule", (format) => {
+      const mol = this.sketcher.getMolecule();
+      let data;
+
+      switch (format) {
+        case "mol":
+          data = mol.toMolfile();
+          break;
+        case "smiles":
+          data = mol.toSMILES();
+          break;
+        default:
+          data = "Unsupported format";
+      }
+
+      // Send to LiveView
+      this.pushEvent("molecule_data", { format, data });
+    });
+  }
+}
+ 
