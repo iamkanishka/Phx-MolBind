e91a49c094c68e235a6137d99b2ceea2a5161e4b
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-05-12 18:27:35 +0530

    feat: Added renderSMILESmolZoomer hook with template

---
+++ lib/phx_molbind_web/live/components_live/molecule_render/molecule_render.ex
@@ -0,0 +1,23 @@
+defmodule PhxMolbindWeb.ComponentsLive.MoleculeRender.MoleculeRender do
+  use PhxMolbindWeb, :live_component
+
+  def render(assigns) do
+    ~H"""
+    <div class="flex flex-row justify-center items start">
+      <canvas
+        id={@id}
+        phx-hook="renderSMILESmolZoomer"
+        data-smiles={@smiles_structure}
+        width="400"
+        height="400"
+      >
+      </canvas>
+    </div>
+    """
+  end
+
+  @spec update(maybe_improper_list() | map(), any()) :: {:ok, any()}
+  def update(assigns, socket) do
+    {:ok, socket |> assign(assigns)}
+  end
+end
