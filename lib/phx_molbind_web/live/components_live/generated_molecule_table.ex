d0a839c8b40b04dcac93d635fda4e34fe739f17c
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-05-13 21:02:27 +0530

    GeneratedMoleculeList

---
+++ lib/phx_molbind_web/live/components_live/generated_molecule_table.ex
@@ -0,0 +1,15 @@
+defmodule PhxMolbindWeb.ComponentsLive.GeneratedMoleculeList do
+  use PhxMolbindWeb, :live_component
+
+  def render(assigns) do
+    ~H"""
+    <div >
+    </div>
+
+    """
+  end
+
+  def update(assigns, socket) do
+    {:ok, socket |> assign(assigns)}
+  end
+end
