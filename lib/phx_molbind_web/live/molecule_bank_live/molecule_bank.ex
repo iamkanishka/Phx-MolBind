f72bb3bc8ab0f9cddc7a05274c4edd390c52b58d
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-05-14 21:53:11 +0530

    chore: Added MainLayout wrapper

--- lib/phx_molbind_web/live/molecule_bank_live/molecule_bank.ex
+++ lib/phx_molbind_web/live/molecule_bank_live/molecule_bank.ex
@@ -2,6 +2,7 @@ defmodule PhxMolbindWeb.MoleculeBankLive.MoleculeBank do
   use PhxMolbindWeb, :live_view
 
   @molecule_bank [
+
     %{
       molecule_name: "Aspirin",
       smiles_structure: "CC(=O)OC1=CC=CC=C1C(O)=O",
@@ -67,56 +68,57 @@ defmodule PhxMolbindWeb.MoleculeBankLive.MoleculeBank do
   @impl true
   def render(assigns) do
     ~H"""
-    <div class="rounded-lg border border-stroke bg-white px-5 pb-2.5 pt-6 shadow-default dark:border-[#181818] dark:bg-[#181818] sm:px-7.5 xl:pb-1">
-      <h4 class="mb-6 text-xl font-semibold text-black dark:text-white">
-        Molecules
-      </h4>
+    <.live_component module={PhxMolbindWeb.ComponentsLive.MainLayout} id={:main_layout}>
+      <div class="rounded-lg border border-stroke bg-white px-5 pb-2.5 pt-6 shadow-default dark:border-[#181818] dark:bg-[#181818] sm:px-7.5 xl:pb-1">
+        <h4 class="mb-6 text-xl font-semibold text-black dark:text-white">
+          Molecules
+        </h4>
 
-      <form phx-change="search_molecule">
-        <input
-          type="search"
-          placeholder="Search molecule"
-          name="search_query"
-          value={@search_query}
-          phx-debounce="300"
-          class="border-gray-300 text-gray-700 placeholder-gray-400 dark:border-gray-600 dark:placeholder-gray-500 text-md mb-4 w-full rounded-lg border bg-white px-4 py-3 shadow-sm outline-none focus:border-primary focus:ring-primary dark:bg-[#181818] dark:text-white"
-        />
-      </form>
+        <form phx-change="search_molecule">
+          <input
+            type="search"
+            placeholder="Search molecule"
+            name="search_query"
+            value={@search_query}
+            phx-debounce="300"
+            class="border-gray-300 text-gray-700 placeholder-gray-400 dark:border-gray-600 dark:placeholder-gray-500 text-md mb-4 w-full rounded-lg border bg-white px-4 py-3 shadow-sm outline-none focus:border-primary focus:ring-primary dark:bg-[#181818] dark:text-white"
+          />
+        </form>
 
-      <div class="flex flex-col">
-        <div class="grid grid-cols-3 rounded-lg bg-gray-2 dark:bg-[#121212] sm:grid-cols-4">
-          <div class="p-2.5 xl:p-5">
-            <h5 class="text-sm font-medium uppercase xsm:text-base">Molecule name</h5>
-          </div>
-
-          <div class="p-2.5 text-center xl:p-5">
-            <h5 class="text-sm font-medium uppercase xsm:text-base">Smile Structure Image</h5>
-          </div>
-
-          <div class="p-2.5 text-center xl:p-5">
-            <h5 class="text-sm font-medium uppercase xsm:text-base">Molecular Weights (g/mol)</h5>
-          </div>
-
-          <div class="hidden p-2.5 text-center sm:block xl:p-5">
-            <h5 class="text-sm font-medium uppercase xsm:text-base">Category Usage</h5>
-          </div>
-        </div>
-
-        <%= for {molecule, index} <- Enum.with_index(@filtered_molecules) do %>
-          <div class={"grid grid-cols-3 sm:grid-cols-4 #{if index != length(@filtered_molecules) - 1, do: "border-b border-stroke dark:border-strokedark", else: ""}"}>
-            <div class="flex items-center justify-center p-2.5 xl:p-5">
-              <p class="text-black dark:text-white">
-                {molecule.molecule_name}
-              </p>
+        <div class="flex flex-col">
+          <div class="grid grid-cols-3 rounded-lg bg-gray-2 dark:bg-[#121212] sm:grid-cols-4">
+            <div class="p-2.5 xl:p-5">
+              <h5 class="text-sm font-medium uppercase xsm:text-base">Molecule name</h5>
             </div>
 
-            <div
-              class="flex items-center gap-3 p-2.5 xl:p-5"
-              phx-click="enlarge_molecule"
-              phx-value-smiles={"#{molecule.smiles_structure}"}
-            >
-              <div class="flex-shrink-0 cursor-pointer">
-                <%!-- <.molecule_structure id={index} structure={molecule.smiles_structure} />
+            <div class="p-2.5 text-center xl:p-5">
+              <h5 class="text-sm font-medium uppercase xsm:text-base">Smile Structure Image</h5>
+            </div>
+
+            <div class="p-2.5 text-center xl:p-5">
+              <h5 class="text-sm font-medium uppercase xsm:text-base">Molecular Weights (g/mol)</h5>
+            </div>
+
+            <div class="hidden p-2.5 text-center sm:block xl:p-5">
+              <h5 class="text-sm font-medium uppercase xsm:text-base">Category Usage</h5>
+            </div>
+          </div>
+
+          <%= for {molecule, index} <- Enum.with_index(@filtered_molecules) do %>
+            <div class={"grid grid-cols-3 sm:grid-cols-4 #{if index != length(@filtered_molecules) - 1, do: "border-b border-stroke dark:border-strokedark", else: ""}"}>
+              <div class="flex items-center justify-center p-2.5 xl:p-5">
+                <p class="text-black dark:text-white">
+                  {molecule.molecule_name}
+                </p>
+              </div>
+
+              <div
+                class="flex items-center gap-3 p-2.5 xl:p-5"
+                phx-click="enlarge_molecule"
+                phx-value-smiles={"#{molecule.smiles_structure}"}
+              >
+                <div class="flex-shrink-0 cursor-pointer">
+                  <%!-- <.molecule_structure id={index} structure={molecule.smiles_structure} />
                 <canvas
                   id={"molecule-canvas-#{index}"}
                   data-smiles={molecule.smiles_structure}
@@ -126,44 +128,50 @@ defmodule PhxMolbindWeb.MoleculeBankLive.MoleculeBank do
                 >
                     </canvas>
                 --%>
-                <canvas
-                  id={"smiles-canvas-#{index}"}
-                  phx-hook="renderSMILESmol"
-                  data-smiles={molecule.smiles_structure}
-                  width="200"
-                  height="200"
-                  data-width="200"
-                  data-height="200"
-                >
-                </canvas>
+                  <canvas
+                    id={"smiles-canvas-#{index}"}
+                    phx-hook="renderSMILESmol"
+                    data-smiles={molecule.smiles_structure}
+                    width="200"
+                    height="200"
+                    data-width="200"
+                    data-height="200"
+                  >
+                  </canvas>
+                </div>
+              </div>
+
+              <div class="hidden items-center justify-center p-2.5 sm:flex xl:p-5">
+                <p class="text-black dark:text-white">
+                  {molecule.molecular_weight}
+                </p>
+              </div>
+
+              <div class="hidden items-center justify-center p-2.5 sm:flex xl:p-5">
+                <p class="text-black dark:text-white">
+                  {molecule.category_usage}
+                </p>
               </div>
             </div>
-
-            <div class="hidden items-center justify-center p-2.5 sm:flex xl:p-5">
-              <p class="text-black dark:text-white">
-                {molecule.molecular_weight}
-              </p>
-            </div>
-
-            <div class="hidden items-center justify-center p-2.5 sm:flex xl:p-5">
-              <p class="text-black dark:text-white">
-                {molecule.category_usage}
-              </p>
-            </div>
-          </div>
-        <% end %>
+          <% end %>
+        </div>
       </div>
-    </div>
 
-    <%= if @expand_molecule and  @selecetd_smiles !=nil do %>
-      <.modal id="molecule-modal" show on_cancel={JS.patch(~p"/molecule-bank")}>
-        <.live_component
-          module={PhxMolbindWeb.ComponentsLive.MoleculeRender.MoleculeRender}
-          id="selected_smiles-canvas"
-          smiles_structure={@selecetd_smiles}
-        />
-      </.modal>
-    <% end %>
+      <%= if @expand_molecule and  @selecetd_smiles !=nil do %>
+        <.modal id="molecule-modal" show on_cancel={JS.patch(~p"/molecule-bank")}>
+          <%!-- <.live_component
+            module={PhxMolbindWeb.ComponentsLive.MoleculeRender.MoleculeRender}
+            id="selected_smiles-canvas"
+            smiles_structure={@selecetd_smiles}
+          /> --%>
+          <.live_component
+            module={PhxMolbindWeb.ComponentsLive.MoleculeRender.ThreedmoleculeRender}
+            id="threed_selected_smiles-canvas"
+            smiles_structure={@selecetd_smiles}
+          />
+        </.modal>
+      <% end %>
+    </.live_component>
     """
   end
 
@@ -194,6 +202,12 @@ defmodule PhxMolbindWeb.MoleculeBankLive.MoleculeBank do
      |> assign(:search_query, query)
      |> assign(:filtered_molecules, filtered)}
   end
+
+
+  @impl true
+def handle_params(_params, _uri, socket) do
+  {:noreply, socket}
+end
 
   @impl true
   def handle_event("enlarge_molecule", %{"smiles" => smiles}, socket) do
